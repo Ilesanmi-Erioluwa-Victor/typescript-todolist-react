edfa7eb43752998ef75c898cdd53cc471408f2ef
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-08 23:03:29 +0100

    Creating store component

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,4 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 export const store = configureStore({
-  reducer: {},
+    reducer: {},
+    middleware(getDefaultMiddleware) {
+        
+    },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AddDispatch = typeof store.dispatch;
