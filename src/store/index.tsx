e66784eb60b33f37412697adbb7084fa3e5e126f
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-08 23:13:58 +0100

    Creating store component

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import tasksReducer, { tasksMiddleware } from "./Tasks.store";
+import modalReducer from "./Modal.store";
+import menuReducer from "./Menu.store";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: { tasks: tasksReducer, modal: modalReducer, menu: menuReducer },
   middleware: (getDefaultMiddleware: any) =>
     getDefaultMiddleware().concat(tasksMiddleware),
 });
