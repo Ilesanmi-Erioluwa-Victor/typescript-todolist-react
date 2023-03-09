53f3e35569ff3ef277592b2bfd7d61daae120dd2
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-09 02:14:07 +0100

    Working on TasksSection folder

--- src/components/Routes/TodaysTasks.tsx
+++ src/components/Routes/TodaysTasks.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import useDescriptionTitle from "../hooks/useDescriptionTitle";
 import useTodayTasks from "../hooks/useTodayTasks";
-import LayoutRoutes from "../Utilities/LayoutRoutes";
+import LayoutRoutes from "components/Utils/LayoutRoutes";
 
 const TodaysTasks: React.FC = () => {
   const todaysTasks = useTodayTasks();
