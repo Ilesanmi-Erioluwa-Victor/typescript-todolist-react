2c9d7c30b10387e51de02d9727ca4af172769bfe
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-09 03:17:22 +0100

    got some bugs on codebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
 import React from "react";
+import AccountData from "./components/AccountSection/AccountData";
+import Footer from "components/Footer";
+import Menu from "components/Menu/Menu";
+import TasksSection from "components/TasksSection/TasksSection";
+import ModalCreateTask from "components/Utils/ModalTask";
+import { Task } from "interface";
+import { useAppDispatch, useAppSelector } from "store/hook";
+import { modalActions } from "./store/Modal.store";
+import { tasksActions } from "./store/Tasks.store";
 
-function App() {
+const App: React.FC = () => {
+  const modal = useAppSelector((state) => state.modal);
+
+  const dispatch = useAppDispatch();
+
+  const closeModalCreateTask = () => {
+    dispatch(modalActions.closeModalCreateTask());
+  };
+
+  const createNewTaskHandler = (task: Task) => {
+    dispatch(tasksActions.addNewTask(task));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      </header>
+    <div className="bg-slate-200 min-h-screen text-slate-600 dark:bg-slate-900 dark:text-slate-400 xl:text-base sm:text-sm text-xs">
+      {modal.modalCreateTaskOpen && (
+        <ModalCreateTask
+          onClose={closeModalCreateTask}
+          nameForm="Add a task"
+          onConfirm={createNewTaskHandler}
+        />
+      )}
+      <Menu />
+      <TasksSection />
+      <Footer />
+      <AccountData />
     </div>
   );
-}
+};
 
 export default App;
