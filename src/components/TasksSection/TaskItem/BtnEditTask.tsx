35a4a44b8b1f6dc2f2c2c95d93f77d34d88bfd7c
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-09 02:07:08 +0100

    Working on TasksSection folder

--- src/components/TasksSection/TaskItem/BtnEditTask.tsx
+++ src/components/TasksSection/TaskItem/BtnEditTask.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useAppDispatch } from "store/hook";
 import { tasksActions } from "store/Tasks.store";
 import ModalCreateTask from "components/Utils/ModalTask";
-import { ReactComponent as OptionsSvg } from "../../../assets/options.svg";
+import { SlOptionsVertical } from "react-icons/sl"
 import { Task } from "interface";
 
 const BtnEditTask: React.FC<{ task: Task }> = ({ task }) => {
@@ -28,7 +28,7 @@ const BtnEditTask: React.FC<{ task: Task }> = ({ task }) => {
         className="transition w-7 sm:w-8 h-6 sm:h-8 grid place-items-center dark:hover:text-slate-200 hover:text-slate-700"
         onClick={openModalEditTask}
       >
-        <OptionsSvg className="w-4 sm:w-5 h-4 sm:h-5" />
+        <SlOptionsVertical className="w-4 sm:w-5 h-4 sm:h-5" />
       </button>
       {modalEditTaskOpen && (
         <ModalCreateTask
